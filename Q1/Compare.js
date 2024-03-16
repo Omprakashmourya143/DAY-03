122eb84ef4e9253fb5ac0accd31eea083b001516
Omprakash <om67048587@gmail.com>
2024-03-16 20:28:01 +0530

    Task file Uploaded

---
+++ Q1/Compare.js
@@ -0,0 +1,17 @@
+// How to compare two JSON have the same properties without order?
+  
+// let obj1 = {name:"person 1",age:5}
+// let obj2 = {age:5,name:"person 1"}
+// let compare = (obj1 == obj2)
+// console.log(compare)
+
+// Output : false
+
+// using length.
+
+// let obj1 = {name:"person 1",age:5}
+// let obj2 = {age:5,name:"person 1"}
+// let compare = (obj1.length == obj2.length)
+// console.log(compare)
+
+// Output : true
